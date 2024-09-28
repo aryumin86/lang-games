eee836c4a71d668d658953390a2e02f244d27712
Алексей Рюмин <aryumin@aryumin.ru>
2024-09-28 12:19:52 +0300

    base repos, models etc added

---
+++ crosswords/CrosswordsWA/Crosswords.Common/Entities/WordDefinition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crosswords.Common.Entities
+{
+    [Table("wordsdefenitions")]
+    public class WordDefinition
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("wordid")]
+        public int WordId { get; set; }
+
+        [Column("definition")]
+        public string? Definition { get; set; }
+
+        [ForeignKey(nameof(LanguageId))]
+        [Column("languageid")]
+        public Language Language { get; set; }
+
+        public int LanguageId { get; set; }
+    }
+}
