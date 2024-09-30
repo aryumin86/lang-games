3110f29ecae9e81dfa40c276b291c6a7d57141e4
Алексей Рюмин <aryumin@aryumin.ru>
2024-09-30 10:04:01 +0300

    crossword base and styles updates

---
+++ crosswords/CrosswordsWA/CrosswordsWA/Models/Crossword/CharCell.cs
@@ -0,0 +1,15 @@
+﻿namespace CrosswordsWA.Models.Crossword
+{
+    public class CharCell
+    {
+        public char? RealLetter { get; set; }
+        public char? UserLetter { get; set; }
+        public bool ContainsLetter { get; set; } = false;
+        public bool ShowLetter { get; set; } = false;
+        public bool? IsCorrect { get; set; }
+        public bool WordStart { get; set; } = false;
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public int? WordNumberInList { get; set; }
+    }
+}
